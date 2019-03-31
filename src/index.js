a078423ce7bbe258faa3a71242c01da0cb75463d
maliaoMJ <1302151931@qq.com>
2019-03-31 15:16:46 +0800

    format system and recover

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ import css from "./index.scss"
 console.log(logo);
 console.log(css);
 console.log('xxx');
+var arr = [32,53,423,2135,3,23,[2353,3534,23,23,[343,353,434,[533,22],3453],34]];
+// 数组扁平化
+function flattenArr(arr){
+    return arr.reduce((result, item) => {
+        return result.concat(Array.isArray(item) ? flattenArr(item) : item)
+    }, [])
+}
+console.log(flattenArr(arr));
